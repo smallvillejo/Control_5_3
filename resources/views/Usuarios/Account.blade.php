8e3f8a9a34cb227363b4c1ec667051efd5294b82
smallvillejo <jorge_9128@hotmail.com>
2017-02-25 17:39:46 -0500

    mensaje

--- resources/views/Usuarios/Account.blade.php
+++ resources/views/Usuarios/Account.blade.php
@@ -37,7 +37,7 @@
 				<i class="fa fa-phone-square" aria-hidden="true"></i> Telefono Empresa:
 			</div>
 			<div class="form-group col-sm-8">
-				<input type="text" name="DireccionEmpresa" id="DireccionEmpresa" class="form-control" placeholder="Ingresa el número telefónico de la empresa">
+				<input type="text" name="TelefonoEmpresa" id="TelefonoEmpresa" class="form-control" placeholder="Ingresa el número telefónico de la empresa">
 			</div>
 		</div>
 		<div class="row">
@@ -45,7 +45,7 @@
 				<i class="fa fa-envelope" aria-hidden="true"></i> Correo Empresa:
 			</div>
 			<div class="form-group col-sm-8">
-				<input type="text" name="DireccionEmpresa" id="DireccionEmpresa" class="form-control" placeholder="Ingresa el email de la empresa">
+				<input type="text" name="EmailEmpresa" id="EmailEmpresa" class="form-control" placeholder="Ingresa el email de la empresa">
 			</div>
 		</div>
 		<div class="row">
@@ -53,32 +53,36 @@
 				<i class="fa fa-picture-o" aria-hidden="true"></i> Logo Empresa:
 			</div>
 			<div class="form-group col-sm-8">
-				<input type="text" name="DireccionEmpresa" id="DireccionEmpresa" class="form-control" placeholder="Ingresa logo de la empresa">
+				<input type="file" name="imagenProducto" class="form-control" id="catagry_logo" accept="image/jpeg, image/jpg,image/png" placeholder="Ingresa logo de la empresa" style="background-color: #32045e; color:#ffffff; " />
+				<span class="help-block">Solo se permiten formatos: JPG,JPEG y PNG.</span>        
 			</div>
-		</div>
-		<hr style="color: #0056b2;" />
+		</div>		
+		
 		<div class="row">
 			<div class="form-group col-sm-4">
-				<i class="fa fa-wrench" aria-hidden="true"></i> Ruta Carpeta Descarga: <a href="#" data-toggle="popover" data-placement="top" title="Se recomienda seleccionar un lugar seguro como DROPBOX donde pueda almacenar datos de su empresa como imágenes de sus mercancías y copias de seguridad del sistema. ">?</a>
+				<i class="fa fa-wrench" aria-hidden="true"></i>
+				Ruta Carpeta Descarga:
+				<a href="#" data-toggle="popover" data-placement="top" title="Se recomienda seleccionar un lugar seguro como DROPBOX donde pueda almacenar datos de su empresa como imágenes de sus mercancías y copias de seguridad del sistema. ">?</a>
 			</div>
-			<div class="form-group col-sm-8">
-				<input type="text" name="DireccionEmpresa" id="DireccionEmpresa" class="form-control" placeholder="Selecciona la carpeta descarga">								
-			</div>
-		</div>
-		<div class="row">
-			<div class="form-group col-sm-8 col-md-offset-4">
-				<button type="button" class="btn btn-succes" id="BtnRegistrar" style="background-color: #32045e" title="Guardar Cambios">
-					<strong> <font size ="2", color ="#ffffff" face="Lucida Sans"><span>Registrar</span></font></strong>
-					<strong> <font size ="2", color ="#ffffff"><span class="fa fa-plus-square"></span></font></strong>
-				</button> 
-			</div>
-		</div> 			
-		<div class="progress">
-			<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 55%; background-color: #32045e" >
-				<span >55% Completado</span>
-			</div>
+			<div class="form-group col-sm-8">				
+				<input type="file" name="RutaArchivo" class="form-control" id="catagry_logo" webkitdirectory directory multiple placeholder="Selcciona Ruta de Archivos" style="background-color: #32045e; color:#ffffff; " />					        
+			</div>								
 		</div>
 	</div>
+	<div class="row">
+		<div class="form-group col-sm-8 col-md-offset-4">
+			<button type="button" class="btn btn-succes" id="BtnRegistrar" style="background-color: #32045e" title="Guardar Cambios">
+				<strong> <font size ="2", color ="#ffffff" face="Lucida Sans"><span>Registrar</span></font></strong>
+				<strong> <font size ="2", color ="#ffffff"><span class="fa fa-plus-square"></span></font></strong>
+			</button> 
+		</div>
+	</div> 			
+	<div class="progress">
+		<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 55%; background-color: #32045e" >
+			<span >55% Completado</span>
+		</div>
+	</div>
+</div>
 </div>
 
 <script>
